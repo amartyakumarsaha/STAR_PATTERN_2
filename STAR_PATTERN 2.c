7bdb8c0bba057d9211847e8b4501c1b215f35e95
Amartya_Kumar_Saha <68885990+amartyakumarsaha@users.noreply.github.com>
2020-11-04 01:17:23 +0530

    Add files via upload

---
+++ STAR_PATTERN 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define row 4
+#define col 4
+int main(void)
+{
+	int i,j,a[row][col];
+	for(i=0;i<row;i++)
+	{
+		for(j=0;j<i;j++)
+		{
+			printf(" ");
+		}
+		for(j=0;j<col;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
